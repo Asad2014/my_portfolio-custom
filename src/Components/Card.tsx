ce3bba528efb10187ee5b3f092aa6304a4db5885
asad2014 <asadkk2014@gmail.com>
2024-11-15 00:31:25 +0500

    first commit

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,38 +1,59 @@
-import React from 'react'
+
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import '../app/styles/card.css';
 
 interface propsType {
-    title: string;
-    desc: string;
-    img: string;
-    tags: string[];
+  title: string;
+  desc: string;
+  img: string;
+  tags: string[];
 }
 
-const Card:React.FC<propsType> = ({ title, desc, img, tags}) => {
-  return (
-    <div className={`card ${window.innerWidth >= 640 ? 'card-sm' : ''}`} data-aos="zoom-in-up">
-    <div>
-    <Image className={`card-image ${window.innerWidth >= 640 ? 'card-image-sm' : ''}`} 
-    src={img}
-    width={350}
-    height={350}
-    alt={title}
-    />
-    </div>
+const Card: React.FC<propsType> = ({ title, desc, img, tags }) => {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
 
-    <div className='card-content'>
-        <div className='card-title'>{title}</div>
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth >= 640);
+    };
+
+    
+    handleResize();
+
+    
+    window.addEventListener('resize', handleResize);
+
+    
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <div className={`card ${isSmallScreen ? 'card-sm' : ''}`} data-aos="zoom-in-up">
+      <div>
+        <Image
+          className={`card-image ${isSmallScreen ? 'card-image-sm' : ''}`}
+          src={img}
+          width={350}
+          height={350}
+          alt={title}
+        />
+      </div>
+
+      <div className="card-content">
+        <div className="card-title">{title}</div>
         <div>{desc}</div>
         <div>
-            {tags.map((el) => (
-                <div className='card-tags' key={el}>
-                    {el}
-                </div>))}
+          {tags.map((el) => (
+            <div className="card-tags" key={el}>
+              {el}
+            </div>
+          ))}
         </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
